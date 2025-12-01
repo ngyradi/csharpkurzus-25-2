4a8b61601b5410c3cf1659076b07ad773351dbc6
Ádám Nagygyörgy <nagyradam@gmail.com>
2025-12-01 20:22:03 +0100

    Setup UI Tests project

---
+++ Hf/TodoSln/Todo.UI.Tests/AddTodoViewTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.UI.Tests
+{
+    internal class AddTodoViewTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
